c2cadea3cc6c74488b81e47891b5364c82b12065
ShawnArnold1 <shawnthearnold@gmail.com>
2018-12-07 15:22:33 -0600

    Re Arranged variables and functions in an order that makes sense

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
 "use strict"
 
-// this function is a subcomponant of a loop
-function applyRandomClassFrom(styles){
-  var r = Math.floor( (Math.random()*100)%6 + 1) -1;
-  /* debug */ console.log(r);
-  return styles[r];
+// Don't move this object
+// The following listeners will create objects or search for objects based on
+//  the state of this object.
+// It gets updated in the dropdownListener(...) function
+var dropdownSelectState = {
+    search: {
+        option: "",
+    },
+    add: {
+        option: "",
+    },
 }
 
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+
+var styles = [
+    'roast-option0',
+    'roast-option1',
+    'roast-option2',
+    'roast-option3',
+    'roast-option4',
+    'roast-option5',
+    'roast-option6',
+];
 
 // renders the html for each individual coffee item
 // This function is a mini css compiler
@@ -19,51 +52,38 @@ function renderCoffeeItem(coffee, styles) {
     var li = "<div class=\"row-48 coffee-item\">\n";
     li += "<span class=\"column col-50 coffee-item-name\">" + coffee.name + "</span>";
     li += "<span class=\"column col-50 coffee-item-roast "+ applyRandomClassFrom(styles) +"\">"
-          + coffee.roast
+        + coffee.roast
         + "</span>";
     li += "</div>\n";
     return li;
 }
 
-// renders the html for an entire list of coffee items
-function renderCoffeeList(coffeeList, styles) {
-    var ul = "<div class=\"row coffee-item\">\n";
-    coffeeList.forEach(function(item){
-      ul += renderCoffeeItem(item, styles);
-    });
-    ul += "</div>";
-    return ul;
+// this function is a subcomponant of a loop
+function applyRandomClassFrom(styles){
+    var r = Math.floor( (Math.random()*100)%6 + 1) -1;
+    /* debug */ console.log(r);
+    return styles[r];
 }
+
+//Assigns the coffee items to columns
 
 function initCoffeeTable(coffeeList){
-  var coffeeTable = {
-    rightCol: [],
-    leftCol: [],
-  };
+    var coffeeTable = {
+        rightCol: [],
+        leftCol: [],
+    };
 
-  for(var i = 0; i < coffeeList.length; i+=2){
-    coffeeTable.leftCol.push(coffeeList[i]);
-    if(i + 1 < coffeeList.length){
-      coffeeTable.rightCol.push(coffeeList[i + 1]);
+    for(var i = 0; i < coffeeList.length; i+=2){
+        coffeeTable.leftCol.push(coffeeList[i]);
+        if(i + 1 < coffeeList.length){
+            coffeeTable.rightCol.push(coffeeList[i + 1]);
+        }
     }
-  }
 
-  return coffeeTable;
+    return coffeeTable;
 }
 
-// Don't move this object
-// The following listeners will create objects or search for objects based on
-//  the state of this object.
-// It gets updated in the dropdownListener(...) function
-var dropdownSelectState = {
-  search: {
-    option: "",
-  },
-  add: {
-    option: "",
-  },
-}
-
+//Assigns dropdowns to default state
 function initdropdownSelectState(){
     dropdownSelectState = {
         search: {
@@ -75,18 +95,79 @@ function initdropdownSelectState(){
     }
 }
 
-var textfieldSelect = function textFieldListener(event){
-    switch(event.target.id){
-      case "rSearch0":
-        /* debug */console.log("0");
-        break;
-      case "rSearch1":
-        /* debug */console.log("1");
-        break;
-      default:
-        /* debug */console.log('option not found');
-        break;
+
+
+// renders the html for an entire list of coffee items
+function renderCoffeeList(coffeeList, styles) {
+    var ul = "<div class=\"row coffee-item\">\n";
+    coffeeList.forEach(function(item){
+      ul += renderCoffeeItem(item, styles);
+    });
+    ul += "</div>";
+    return ul;
+}
+
+
+function updateCoffeeList(table, styles) {
+    var leftCol = document.getElementById("coffee-list-col-0");
+    var rightCol = document.getElementById("coffee-list-col-1");
+    leftCol.innerHTML = renderCoffeeList(table.leftCol, styles);
+    rightCol.innerHTML = renderCoffeeList(table.rightCol, styles);
+}
+
+
+// updates the dropdownSelectState object
+var dropdownSelect = function dropdownListener(event){
+
+    console.log(event.target);
+
+    switch(event.target.name){
+        case "roast-types-search":
+            dropdownSelectState.search.option = event.target.value;
+            break;
+        case "roast-types-add":
+            dropdownSelectState.add.option = event.target.value;
+            break;
     }
+
+    console.log(dropdownSelectState);
+}
+
+
+function searchCoffees(query, coffeeList){
+
+    var result = [];
+
+    if(query.option == ""){
+        alert("Please select from: all, light, medium, or dark.");
+    }
+    else if(query.option == "All"){
+        coffeeList.forEach(function(coffee){
+            if(coffee
+                    .name
+                    .toLowerCase()
+                    .substring(0, query.str.length)
+                === query.str
+            ){
+                result.push(coffee);
+            }
+        });
+    }
+    else {
+        coffeeList.forEach(function(coffee){
+            if( (coffee.name.toLowerCase().substring(0, query.str.length) === query.str)
+                && (coffee.roast === query.option)
+            ){
+                result.push(coffee);
+            }
+        });
+    }
+
+    return result;
+}
+
+function addCoffee(coffeeList, coffee){
+    coffeeList.push(coffee);
 }
 
 var btnSelect = function btnListener(event){
@@ -124,93 +205,6 @@ var btnSelect = function btnListener(event){
   }
 }
 
-// updates the dropdownSelectState object
-var dropdownSelect = function dropdownListener(event){
-
-  console.log(event.target);
-
-  switch(event.target.name){
-    case "roast-types-search":
-      dropdownSelectState.search.option = event.target.value;
-      break;
-    case "roast-types-add":
-      dropdownSelectState.add.option = event.target.value;
-      break;
-  }
-
-  console.log(dropdownSelectState);
-}
-
-function updateCoffeeList(table, styles) {
-    var leftCol = document.getElementById("coffee-list-col-0");
-    var rightCol = document.getElementById("coffee-list-col-1");
-    leftCol.innerHTML = renderCoffeeList(table.leftCol, styles);
-    rightCol.innerHTML = renderCoffeeList(table.rightCol, styles);
-}
-
-function searchCoffees(query, coffeeList){
-
-  var result = [];
-
-  if(query.option == ""){
-    alert("Please select from: all, light, medium, or dark.");
-  }
-  else if(query.option == "All"){
-    coffeeList.forEach(function(coffee){
-      if(coffee
-          .name
-          .toLowerCase()
-          .substring(0, query.str.length)
-          === query.str
-        ){
-          result.push(coffee);
-        }
-    });
-  }
-  else {
-    coffeeList.forEach(function(coffee){
-      if( (coffee.name.toLowerCase().substring(0, query.str.length) === query.str)
-          && (coffee.roast === query.option)
-        ){
-          result.push(coffee);
-        }
-    });
-  }
-
-  return result;
-}
-
-function addCoffee(coffeeList, coffee){
-  coffeeList.push(coffee);
-}
-
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
-
-var styles = [
-    'roast-option0',
-    'roast-option1',
-    'roast-option2',
-    'roast-option3',
-    'roast-option4',
-    'roast-option5',
-    'roast-option6',
-];
 
 initdropdownSelectState();
 
